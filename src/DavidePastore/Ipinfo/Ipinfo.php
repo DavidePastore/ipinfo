18ca58d763c968986c0202bb8075e35d41142542
davidepastore <pasdavide@gmail.com>
2014-10-09 14:12:06 +0200

    Add files.

---
+++ src/DavidePastore/Ipinfo/Ipinfo.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace DavidePastore\Ipinfo;
+
+/**
+ * ipinfo.io service wrapper.
+ * @author davidepastore
+ *
+ */
+class Ipinfo {
+	
+	/**
+	 * The base url of the ipinfo service.
+	 * @var string
+	 */
+	const BASE_URL = "http://ipinfo.io/";
+	
+	/**
+	 * The ip string.
+	 * @var string
+	 */
+	const IP = "ip";
+	
+	/**
+	 * The hostname string.
+	 * @var string
+	 */
+	const HOSTNAME = "hostname";
+	
+	/**
+	 * The loc string.
+	 * @var string
+	 */
+	const LOC = "loc";
+	
+	/**
+	 * The org string.
+	 * @var string
+	 */
+	const ORG = "org";
+	
+	/**
+	 * The city string.
+	 * @var string
+	 */
+	const CITY = "city";
+	
+	/**
+	 * The region string.
+	 * @var string
+	 */
+	const REGION = "region";
+	
+	/**
+	 * The country string.
+	 * @var string
+	 */
+	const COUNTRY = "country";
+	
+	/**
+	 * The phone string.
+	 * @var string
+	 */
+	const PHONE = "phone";
+	
+	/**
+	 * The geo string.
+	 * @var string
+	 */
+	const GEO = "geo";
+	
+	/**
+	 * All the settings
+	 * @var array
+	 */
+	protected $settings;
+	
+	/**
+	 * Create an Ipinfo instance.
+	 * @param array $settings An array with all the settings.
+	 * Supported keys are:
+	 * - token: string the developer token;
+	 * - debug: boolean active or not the debug.
+	 */
+	public function __construct($settings = array()){
+		//Merge user settings
+		$this->settings = array_merge(array(
+				'token' => '',
+				'debug' => false
+		), $settings);
+	}
+	
+	
+	/**
+	 * Get all the info about your own ip address.
+	 * @return \DavidePastore\Ipinfo\Host The Host object with all the info.
+	 */
+	public function getYourOwnIpDetails(){
+		$response = $this->makeCurlRequest($this::BASE_URL . "json");
+		$response = json_decode($response, true);
+		return new Host($response);
+	}
+	
+	/**
+	 * Get all the info about an ip address.
+	 * @param string $ipAddress The ip address.
+	 * @return \DavidePastore\Ipinfo\Host The Host object with all the info.
+	 */
+	public function getFullIpDetails($ipAddress){
+		$response = $this->makeCurlRequest($this::BASE_URL . $ipAddress);
+		$response = json_decode($response, true);
+		return new Host($response);
+	}
+	
+	/**
+	 * Get a specific field value.
+	 * @param string $ipAddress The ip address.
+	 * @param string $field The field.
+	 * @return string|\DavidePastore\Ipinfo\Host The value of the given field for the given ip.
+	 * This could returns an Host object if you call it with for the field
+	 * \DavidePastore\Ipinfo\Ipinfo::GEO.
+	 */
+	public function getSpecificField($ipAddress, $field){
+		$response = substr($this->makeCurlRequest($this::BASE_URL . $ipAddress . "/" . $field), 0, -1);
+		if($field == $this::GEO){
+			$response = json_decode($response, true);
+			$response = new Host($response);
+		}
+		return $response;
+	}
+	
+	/**
+	 * Get a specific field value of your own ip address.
+	 * @param string $field The field.
+	 * @return string|\DavidePastore\Ipinfo\Host The value of the given field for your own ip.
+	 * This could returns an Host object if you call it with for the field
+	 * \DavidePastore\Ipinfo\Ipinfo::GEO.
+	 */
+	public function getYourOwnIpSpecificField($field){
+		$response = substr($this->makeCurlRequest($this::BASE_URL . $field), 0, -1);
+		if($field == $this::GEO){
+			$response = json_decode($response, true);
+			$response = new Host($response);
+		}
+		return $response;
+	}
+	
+	/**
+	 * Make a curl request.
+	 * @param string $address The address of the request.
+	 * @return string Returns the response from the request.
+	 */
+	private function makeCurlRequest($address){
+		$curl = curl_init();
+		
+		if(!empty($this->settings['token'])){
+			$address .= "?token=" . $this->settings['token'];
+		}
+		
+		if($this->settings['debug']){
+			echo "Request address: " . $address . "\n";
+		}
+		
+		curl_setopt_array($curl, array(
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_URL => $address
+		));
+		
+		$response = curl_exec($curl);
+		
+		curl_close($curl);
+		
+		return $response;
+	}
+}
---
+++ test/DavidePastore/Ipinfo/IpinfoTest.php
@@ -0,0 +1,43 @@
+<?php
+use DavidePastore\Ipinfo\Ipinfo;
+use DavidePastore\Ipinfo\Host;
+
+/**
+ * Test for the Ipinfo class.
+ * @author davidepastore
+ *
+ */
+class IpinfoTest extends \PHPUnit_Framework_TestCase {
+	
+	/**
+	 * Test full ip details.
+	 */
+	public function testGetFullIpDetails(){
+		$ipinfo = new Ipinfo();
+		$expected = new Host(array(
+			"city"		=>	"Mountain View",
+			"country"	=>	"US",
+			"geo"		=>	"",
+			"hostname"	=>	"google-public-dns-a.google.com",
+			"ip"		=>	"8.8.8.8",
+			"loc"		=>	"37.3860,-122.0838",
+			"org"		=>	"AS15169 Google Inc.",
+			"phone"		=>	"",
+			"region"	=>	"California"
+		));
+		$actual = $ipinfo->getFullIpDetails("8.8.8.8");
+		
+		$this->assertEquals($expected, $actual);
+	}
+	
+	/**
+	 * Test city field value.
+	 */
+	public function testGetSpecificField(){
+		$ipinfo = new Ipinfo();
+		$expected = "Mountain View";
+		$actual = $ipinfo->getSpecificField("8.8.8.8", Ipinfo::CITY);
+	
+		$this->assertEquals($expected, $actual);
+	}
+}
